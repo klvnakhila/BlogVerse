47b7f50d76786d2610f00af05dc63475b260e54e
klvnakhila <klvnakhila@gmail.com>
2025-10-05 19:01:56 +0530

    editing index.js

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use(express.json())
 // }))
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://blogverse-backend-five.vercel.app"
+  "https://blogverse-blogging.vercel.app"
 ];
 
 app.use(cors({
