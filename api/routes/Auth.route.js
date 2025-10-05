db5855ac706af2503d22885a9b8a5e1fd23da168
klvnakhila <klvnakhila@gmail.com>
2025-10-05 21:01:02 +0530

    authentication removed from logout

--- api/routes/Auth.route.js
+++ api/routes/Auth.route.js
@@ -7,6 +7,6 @@ const AuthRoute = express.Router()
 AuthRoute.post('/register', Register)
 AuthRoute.post('/login', Login)
 AuthRoute.post('/google-login', GoogleLogin)
-AuthRoute.get('/logout', authenticate, Logout)
+AuthRoute.get('/logout', Logout)
 
 export default AuthRoute
